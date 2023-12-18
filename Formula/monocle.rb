c01b760d8cd6e98575a86aa9658c413a792d4236
Mingwei Zhang <mingwei@mwzhang.com>
2023-12-18 14:15:05 -0800

    update sha256 for monocle 0.5.2

--- Formula/monocle.rb
+++ Formula/monocle.rb
@@ -5,7 +5,7 @@ class Monocle < Formula
 
   version "0.5.2"
   url "https://github.com/bgpkit/monocle/releases/download/v0.5.2/monocle-universal-apple-darwin.tar.gz"
-  sha256 "7622f6f677259601ab1ae7fbd3f622239be8cbe7bf2a5548be67f9798dbec28b"
+  sha256 "a57995904db73aa0ee1e5ff938f970cfdb67203d78b95e1293ba97d38a247413"
 
   def install
     bin.install "monocle"
