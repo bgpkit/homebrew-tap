e729e6485cede0ed465a4525522d810ba4e855ca
Mingwei Zhang <mingwei@mwzhang.com>
2024-03-20 19:44:21 -0700

    update bgpkit-commons to 0.5.2

--- Formula/bgpkit-commons.rb
+++ Formula/bgpkit-commons.rb
@@ -2,10 +2,10 @@ class BgpkitCommons < Formula
   desc "Tool for accessing common BGP data"
   homepage "https://github.com/bgpkit/bgpkit-commons"
 
-  version "0.5.1"
+  version "0.5.2"
 
-  url "https://github.com/bgpkit/bgpkit-commons/releases/download/v0.5.1/bgpkit-commons-universal-apple-darwin.tar.gz"
-  sha256 "53071634cde680007023dd837a17ef1d2e8ddabe9910f0be3142a8400a76d092"
+  url "https://github.com/bgpkit/bgpkit-commons/releases/download/v0.5.2/bgpkit-commons-universal-apple-darwin.tar.gz"
+  sha256 "20b2b69cc31a0a4ba2de6a8a1b36cc4b501f53eb12f5a66108fc671124b68766"
 
   def install
     bin.install "bgpkit-commons"
