eaee6e8a515bd97389897c52de9ad99f384285db
Mingwei Zhang <mingwei@mwzhang.com>
2024-07-16 18:21:26 -0700

    update oneio to 0.16.9

--- Formula/oneio.rb
+++ Formula/oneio.rb
@@ -2,10 +2,10 @@ class Oneio < Formula
   desc "Rust CLI that can read from anywhere"
   homepage "https://github.com/bgpkit/oneio"
 
-  version "0.16.7"
+  version "0.16.9"
 
-  url "https://github.com/bgpkit/oneio/releases/download/v0.16.7/oneio-universal-apple-darwin.tar.gz"
-  sha256 "ddfda9fd412eb53aed40238dedd21003109a36b1d0afafdac1af601f5eddd3ab"
+  url "https://github.com/bgpkit/oneio/releases/download/v0.16.9/oneio-universal-apple-darwin.tar.gz"
+  sha256 "a7a907284e99ad6b84fc1dec21b867e00b674ffac328e4c108dff0749f9ee99f"
 
   def install
     bin.install "oneio"
