d18139c03e4d293313a2fbbf4bd573b03c7261ca
Mingwei Zhang <mingwei@mwzhang.com>
2024-08-23 08:18:21 -0700

    update bgpkit-broker to v0.7.5

--- Formula/bgpkit-broker.rb
+++ Formula/bgpkit-broker.rb
@@ -3,10 +3,10 @@ class BgpkitBroker < Formula
   desc "BGP MRT files finder"
   homepage "https://github.com/bgpkit/bgpkit-broker"
 
-  version "0.7.4"
+  version "0.7.5"
 
-  url "https://github.com/bgpkit/bgpkit-broker/releases/download/v0.7.4/bgpkit-broker-universal-apple-darwin.tar.gz"
-  sha256 "289e55c54288df0306ac10a66221a776ce5817f344d6f0df574c3c809d4bb627"
+  url "https://github.com/bgpkit/bgpkit-broker/releases/download/v0.7.5/bgpkit-broker-universal-apple-darwin.tar.gz"
+  sha256 "461dbcbca4a86a0db5d1b11bab5669f6a7e51c320157e9fa5ea06e30729b99bb"
 
   def install
     bin.install "bgpkit-broker"
