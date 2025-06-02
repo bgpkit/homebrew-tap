e75fe9751dfe90ba9211367e2a879b8cc0a22519
Mingwei Zhang <mingwei@mwzhang.com>
2025-06-01 23:14:05 -0700

    update asninfo to v0.3.1

--- Formula/asninfo.rb
+++ Formula/asninfo.rb
@@ -3,9 +3,9 @@ class Asninfo < Formula
   desc "ASN data exporter"
   homepage "https://github.com/bgpkit/asninfo"
 
-  version "0.2.1"
-  url "https://github.com/bgpkit/asninfo/releases/download/v0.2.1/asninfo-universal-apple-darwin.tar.gz"
-  sha256 "ebf2687e77eea99c1ec2ea54f80aae68ea6215d8c1ce9787c43cd9427557a177"
+  version "0.3.1"
+  url "https://github.com/bgpkit/asninfo/releases/download/v0.3.1/asninfo-universal-apple-darwin.tar.gz"
+  sha256 "418b52c02871cfcceb6bfdc5d29908f4cad072ee8bceb1d9b0555d1ac38d2f94"
 
   def install
     bin.install "asninfo"
