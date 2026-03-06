9fd3599f7bacb8da2eec4ce4a0ca94cb120ef993
Mingwei Zhang <mingwei@mwzhang.com>
2026-03-06 05:33:59 -0800

    upgrade bgpkit-parser to v0.15.0

--- Formula/bgpkit-parser.rb
+++ Formula/bgpkit-parser.rb
@@ -3,10 +3,10 @@ class BgpkitParser < Formula
   desc "MRT/BGP data CLI parser"
   homepage "https://github.com/bgpkit/bgpkit-parser"
 
-  version "0.12.0"
+  version "0.15.0"
 
-  url "https://github.com/bgpkit/bgpkit-parser/releases/download/v0.12.0/bgpkit-parser-universal-apple-darwin.tar.gz"
-  sha256 "1a245412eead968392a2b0c4311c294ad64d500dbeaa6220ea37338640bc5c17"
+  url "https://github.com/bgpkit/bgpkit-parser/releases/download/v0.15.0/bgpkit-parser-universal-apple-darwin.tar.gz"
+  sha256 "59a8cb7906bef2a72dd8217facbecfc6421917ed7095b764c723ee19db4f3c22"
 
   def install
     bin.install "bgpkit-parser"
