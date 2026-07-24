23d050114fbf2691cf5defe99c6f34426d2b4dac
Mingwei Zhang <miz@fastmail.com>
2026-07-23 17:11:34 -0700

    oneio: update to v0.24.1
    - Add Linux x86_64 and aarch64 platform support
    - Update macOS universal binary to v0.24.1

--- Formula/oneio.rb
+++ Formula/oneio.rb
@@ -2,10 +2,22 @@ class Oneio < Formula
   desc "Rust CLI that can read from anywhere"
   homepage "https://github.com/bgpkit/oneio"
 
-  version "0.21.0"
+  version "0.24.1"
 
-  url "https://github.com/bgpkit/oneio/releases/download/v0.21.0/oneio-universal-apple-darwin.tar.gz"
-  sha256 "442fab118e72753d315e9c24cbc1e00b82aca56e13a5cbeb6352da79e616e48d"
+  on_macos do
+    url "https://github.com/bgpkit/oneio/releases/download/v0.24.1/oneio-universal-apple-darwin.tar.gz"
+    sha256 "5763b146607df2b1ecde6b7cb839053b1f3c625ac97cecd367567968673273c1"
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/bgpkit/oneio/releases/download/v0.24.1/oneio-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "cd282dbca6c35209d0a0c9282dd5ac9ccaddf5bdfce08f1245a7abd71beb769c"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/bgpkit/oneio/releases/download/v0.24.1/oneio-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e5a91b2bf865b76d84163585fffb58d749182b6874aae0a1e2bc321105890920"
+    end
+  end
 
   def install
     bin.install "oneio"
@@ -14,5 +26,4 @@ class Oneio < Formula
   test do
     system "#{bin}/oneio --version"
   end
-
 end
