bd69ae78b8193f1e5c710de892e7ef23b998b11d
Mingwei Zhang <mingwei@mwzhang.com>
2026-08-17 13:34:28 -0700

    update monocle to v1.5.0

--- Formula/monocle.rb
+++ Formula/monocle.rb
@@ -1,8 +1,8 @@
 class Monocle < Formula
   desc "See through all BGP data with a monocle"
   homepage "https://github.com/bgpkit/monocle"
-  url "https://github.com/bgpkit/monocle/releases/download/v1.4.0/monocle-universal-apple-darwin.tar.gz"
-  sha256 "ef20cb77b58922abb7c4b7b5c42ac643261af0e7d1e84da32fd7153edd575e83"
+  url "https://github.com/bgpkit/monocle/releases/download/v1.5.0/monocle-universal-apple-darwin.tar.gz"
+  sha256 "0c0253533ab4a99cdc0fb825b6390ace9eb7f4fb27e6bc23d151ef98b630422b"
 
   def install
     bin.install "monocle"
